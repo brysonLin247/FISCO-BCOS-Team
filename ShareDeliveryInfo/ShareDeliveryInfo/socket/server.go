ba891dffacf96b20eb29c2379faca2348319ea4f
becoder999 <mrjzhao@126.com>
2020-10-20 14:56:41 +0800

    调整代码

--- ShareDeliveryInfo/ShareDeliveryInfo/socket/server.go
+++ ShareDeliveryInfo/ShareDeliveryInfo/socket/server.go
@@ -44,54 +44,56 @@ func connHandler(c net.Conn, session mycontract.ShareDeliveryInfoSession, client
         bytes = append(bytes, currBytes...)
         fmt.Println("读取计数："+strconv.Itoa(read_cnt) + "当前读取字符："+string(buf[0:cnt])+"所有字符："+string(bytes));
 
+        if (read_cnt % 2 == 0) {
+        // //3.3 根据输入流进行逻辑处理
+
+            originStr := string(bytes)
+            fmt.Println("输入字符："+originStr)
+            fmt.Println(bytes)
+
+            fmt.Printf("包裹%s当前到达的站点: %s\n ",string(bytes[0:3]), string(bytes[4:12]))
+
+            package_uid, err := strconv.ParseUint(string(bytes[0:3]),16,64)
+            if err!=nil{
+              log.Fatal(err)
+            }
+            station := string(bytes[4:12])
+
+            // package_uid := uint64(1)
+            // station := "test station"
+
+            fmt.Printf("站点读卡器读取包裹编号->%d, 到达站点: %s \n", package_uid, station)
+            // c.Write([]byte("服务器端回复" + originStr + "\n"))
+
+            setTransaction,err := session.Set(package_uid, station)
+            if err!=nil {
+              log.Fatal(err)
+            }
+            // 等待交易被打包处理
+            receipt, err := client.WaitMined(setTransaction)
+            if err != nil {
+              log.Fatalf("tx mining error:%v\n", err)
+            }
+            fmt.Printf("信息已上链，获得的结果收据: %s\n", receipt.GetTransactionHash())
+
+            s, err := session.Get(package_uid)
+            if err!=nil{
+              log.Fatal(err)
+            }
+
+            fmt.Printf("包裹%d信息已录入站点: %s节点\n ",package_uid, s)
+
+            //c.Close() //关闭client端的连接，telnet 被强制关闭
+
+            // nextStation, err := session.NextStation(s)
+
+            // fmt.Printf("下一步物流站点是：xxxxx",)
+
+            read_cnt = 0;
+            bytes = bytes[:0];
+        }
+
     }
-
-    // //3.3 根据输入流进行逻辑处理
-
-    originStr := string(bytes)
-    fmt.Println("输入字符："+originStr)
-    fmt.Println(bytes)
-
-    fmt.Printf("包裹%s当前到达的站点: %s\n ",string(bytes[9:12]), string(bytes[21:30]))
-
-    package_uid, err := strconv.ParseUint(string(bytes[9:12]),16,64)
-    if err!=nil{
-      log.Fatal(err)
-    }
-    station := string(bytes[21:30])
-
-    // package_uid := uint64(1)
-    // station := "test station"
-
-    fmt.Printf("站点读卡器读取包裹编号->%d, 到达站点: %s \n", package_uid, station)
-    // c.Write([]byte("服务器端回复" + originStr + "\n"))
-
-    setTransaction,err := session.Set(package_uid, station)
-    if err!=nil {
-      log.Fatal(err)
-    }
-    // 等待交易被打包处理
-    receipt, err := client.WaitMined(setTransaction)
-    if err != nil {
-      log.Fatalf("tx mining error:%v\n", err)
-    }
-    fmt.Printf("信息已上链，获得的结果收据: %s\n", receipt.GetTransactionHash())
-
-    s, err := session.Get(package_uid)
-    if err!=nil{
-      log.Fatal(err)
-    }
-
-    fmt.Printf("包裹%d: %s\n ",package_uid, s)
-
-    //c.Close() //关闭client端的连接，telnet 被强制关闭
-
-    // nextStation, err := session.NextStation(s)
-
-    // fmt.Printf("下一步物流站点是：xxxxx",)
-
-    read_cnt = 0;
-    bytes = bytes[:0];
 
 }
 
