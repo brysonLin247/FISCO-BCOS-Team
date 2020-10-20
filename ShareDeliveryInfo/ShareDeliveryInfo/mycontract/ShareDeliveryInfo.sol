721a6e5dc1f789f936f87f7701c7850839b82551
becoder999 <mrjzhao@126.com>
2020-10-20 15:49:22 +0800

    调整代码

--- ShareDeliveryInfo/ShareDeliveryInfo/mycontract/ShareDeliveryInfo.sol
+++ ShareDeliveryInfo/ShareDeliveryInfo/mycontract/ShareDeliveryInfo.sol
@@ -15,19 +15,7 @@ contract ShareDeliveryInfo {
 
     //快递到达新站点，更新信息
     function set(uint64 _pkg_uid, string station) public {
-        delivery_path[_pkg_uid] = strConcat(delivery_path[_pkg_uid], "； 到达站点：");
         delivery_path[_pkg_uid] = strConcat(delivery_path[_pkg_uid], station);
     }
 
-    //用于拼接字符串
-    function strConcat(string _a, string _b) internal returns (string){
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        string memory ret = new string(_ba.length + _bb.length);
-        bytes memory bret = bytes(ret);
-        uint k = 0;
-        for (uint i = 0; i < _ba.length; i++)bret[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) bret[k++] = _bb[i];
-        return string(ret);
-   }
 }
